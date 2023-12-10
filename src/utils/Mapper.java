1c10a51604d0a3a5ca7a0ec710c3a005d91d7e2a
mrsicsasa <sasamrsic20@gmail.com>
2023-12-10 21:33:35 +0100

    Mapper for classes in models osobe v2

--- src/utils/Mapper.java
+++ src/utils/Mapper.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import models.osobe.AngazovanaOsoba;
 import models.osobe.Menadzer;
@@ -16,48 +17,52 @@ public class Mapper {
 public Mapper() {
 	super();
 }
-public ArrayList<Menadzer> konvertujUMenadzer(List<ArrayList<String>>podaci){
-	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-	ArrayList<Menadzer>rezultat= new ArrayList<Menadzer>();
-	for( ArrayList<String>podatak : podaci){
-		LocalDateTime pocetakAngazovanja = LocalDateTime.parse(podatak.get(3), formatter);
-		LocalDateTime krajAngazovanja= LocalDateTime.parse(podatak.get(4), formatter);
-		rezultat.add(new Menadzer(podatak.get(0), podatak.get(1), Integer.parseInt(podatak.get(2)),pocetakAngazovanja,krajAngazovanja,new Uloga(podatak.get(5),podatak.get(6),podatak.get(7))))	;
-	}
-	return rezultat;
+private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+private LocalDateTime parsirajDatum(String datum, DateTimeFormatter formatter) {
+    return LocalDateTime.parse(datum, FORMATTER);
 }
-public ArrayList<Promoter> konvertujUPromotera(List<ArrayList<String>>podaci){
-	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    ArrayList<Promoter> rezultat = new ArrayList<Promoter>();
-
-    for (ArrayList<String> podatak : podaci) {
-        LocalDateTime pocetakAngazovanja = LocalDateTime.parse(podatak.get(3), formatter);
-        LocalDateTime krajAngazovanja = LocalDateTime.parse(podatak.get(4), formatter);
-
-        // Pretpostavljamo da se Menadzer nalazi na indeksu 8 u podacima
-        Menadzer naredjeniMenadzer =new Menadzer();
-        naredjeniMenadzer.setIme(podatak.get(5));
-        int brojPodatakaOOdmorima=podatak.size()-6;
-        ArrayList<Odmor>odmori=new ArrayList<Odmor>();
-        if(brojPodatakaOOdmorima>2) {
-        	for(int i=0;i<brojPodatakaOOdmorima;i=i+2) {
-        		odmori.add(new Odmor(LocalDateTime.parse(podatak.get(i+6),formatter),LocalDateTime.parse(podatak.get(i+7),formatter)));
-        	}
-        }
-        else {
-        	odmori.add(new Odmor(LocalDateTime.parse(podatak.get(6),formatter),LocalDateTime.parse(podatak.get(7),formatter)));
-        }
-        // Dodajemo novu instancu Promoter u rezultat
-        rezultat.add(new Promoter(
-                podatak.get(0),
-                podatak.get(1),
-                Integer.parseInt(podatak.get(2)),
-                pocetakAngazovanja,
-                krajAngazovanja,
-                naredjeniMenadzer,
-                new Odmori(odmori)
-        ));
-    }
-	return rezultat;
+private LocalDateTime konvertujStringUDatum(String datum) {
+    return LocalDateTime.parse(datum, FORMATTER);
 }
+public ArrayList<Menadzer> konvertujUMenadzer(List<ArrayList<String>> podaci) {
+    return podaci.stream()
+            .map(this::konvertujPodatakUMenadzer)
+            .collect(Collectors.toCollection(ArrayList::new));
+}
+
+private Menadzer konvertujPodatakUMenadzer(ArrayList<String> podatak) {
+    return new Menadzer(
+            podatak.get(0),
+            podatak.get(1),
+            Integer.parseInt(podatak.get(2)),
+           parsirajDatum(podatak.get(3), FORMATTER),
+           parsirajDatum(podatak.get(4), FORMATTER),
+            new Uloga(podatak.get(5), podatak.get(6), podatak.get(7))
+    );
+}
+
+public ArrayList<Promoter> konvertujUPromotera(List<ArrayList<String>> podaci) {
+    return podaci.stream()
+            .map(this::konvertujPodatakUPromotera)
+            .collect(Collectors.toCollection(ArrayList::new));
+}
+
+private Promoter konvertujPodatakUPromotera(ArrayList<String> podatak) {
+	Menadzer menazder=new Menadzer();
+	 ArrayList<Odmor> odmori = new ArrayList<>();
+     for (int i = 6; i < podatak.size(); i += 2) {
+         odmori.add(new Odmor(parsirajDatum(podatak.get(i), FORMATTER),parsirajDatum(podatak.get(i+1), FORMATTER)));
+     }
+	menazder.setIme(podatak.get(5));
+    return new Promoter(
+            podatak.get(0),
+            podatak.get(1),
+            Integer.parseInt(podatak.get(2)),
+            LocalDateTime.parse(podatak.get(3), FORMATTER),
+            LocalDateTime.parse(podatak.get(4), FORMATTER),
+            menazder,
+            new Odmori(odmori)
+            );
+}
+
 }
